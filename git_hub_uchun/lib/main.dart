a12a075e3acfae29e60480435e67c00a28095f20
abdullohflutter <abdullohflutter@gmail.com>
2024-11-27 10:48:08 +0500

    imtixon

--- git_hub_uchun/lib/main.dart
+++ git_hub_uchun/lib/main.dart
@@ -1,181 +1,377 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:lottie/lottie.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MusicPlayerScreen(),
+      home: SplashScreen(),
     );
   }
 }
 
-class MusicPlayerScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   @override
-  _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => RegistrationScreen()),
+      );
+    });
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Lottie.asset(
+              'lottie/dollar.json',
+              height: 300,
+              width: 300,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  final List<Map<String, String>> _songs = [
-    {"title": "AKA MAKASI", "artist": "Munisa Rizayeva", "path": "munisa.mp3"},
-    {"title": "TEOTIMUR", "artist": "Artist 2", "path": "ashula.mp3"},
-    {"title": "Ummmon", "artist": "Artist 3", "path": "ummon.mp3"},
-  ];
-  int _currentIndex = 0;
-  bool _isPlaying = false;
-  Duration _currentPosition = Duration.zero;
-  Duration _totalDuration = Duration.zero;
-
-  @override
-  void initState() {
-    super.initState();
-    _audioPlayer.onPositionChanged.listen((position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    });
-    _audioPlayer.onDurationChanged.listen((duration) {
-      setState(() {
-        _totalDuration = duration;
-      });
-    });
-    _audioPlayer.onPlayerComplete.listen((_) {
-      _nextTrack();
-    });
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-
-  void _playPause() async {
-    if (_isPlaying) {
-      await _audioPlayer.pause();
-    } else {
-      await _audioPlayer.play(AssetSource(_songs[_currentIndex]['path']!));
-    }
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
-  }
-
-  void _nextTrack() {
-    if (_currentIndex < _songs.length - 1) {
-      _currentIndex++;
-    } else {
-      _currentIndex = 0;
-    }
-    _playTrack();
-  }
-
-  void _previousTrack() {
-    if (_currentIndex > 0) {
-      _currentIndex--;
-    } else {
-      _currentIndex = _songs.length - 1;
-    }
-    _playTrack();
-  }
-
-  void _playTrack() async {
-    await _audioPlayer.play(AssetSource(_songs[_currentIndex]['path']!));
-    setState(() {
-      _isPlaying = true;
-    });
-  }
+class RegistrationScreen extends StatelessWidget {
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController loginController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        backgroundColor: Color(0xFF7F00FF),
+        elevation: 0,
+        title: Text(
+          "Ro'yxatdan o'tish",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Stack(
         children: [
-          Expanded(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    _songs[_currentIndex]['title']!,
-                    style: TextStyle(color: Colors.white, fontSize: 24),
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    _songs[_currentIndex]['artist']!,
-                    style: TextStyle(color: Colors.grey, fontSize: 18),
-                    textAlign: TextAlign.center,
-                  ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF7F00FF),
+                  Color(0xFFE100FF),
                 ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
             ),
           ),
-          Slider(
-            activeColor: Colors.white,
-            inactiveColor: Colors.grey,
-            min: 0,
-            max: _totalDuration.inSeconds.toDouble(),
-            value: _currentPosition.inSeconds.toDouble(),
-            onChanged: (value) {
-              _audioPlayer.seek(Duration(seconds: value.toInt()));
-            },
+          Center(
+            child: Lottie.asset(
+              'lottie/dollar.json',
+              height: 300,
+              width: 300,
+              fit: BoxFit.cover,
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  _formatDuration(_currentPosition),
-                  style: TextStyle(color: Colors.white),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      _buildTextField("Familiya",
+                          controller: lastNameController),
+                      SizedBox(height: 10),
+                      _buildTextField("Ism", controller: firstNameController),
+                      SizedBox(height: 10),
+                      _buildTextField("Login", controller: loginController),
+                      SizedBox(height: 10),
+                      _buildTextField("Parol",
+                          controller: passwordController, isPassword: true),
+                    ],
+                  ),
                 ),
-                Text(
-                  _formatDuration(_totalDuration),
-                  style: TextStyle(color: Colors.white),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Ma'lumotlar muvaffaqiyatli saqlandi!"),
+                      ),
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(
+                          firstName: firstNameController.text,
+                          lastName: lastNameController.text,
+                        ),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple.shade700,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  ),
+                  child: Text(
+                    "Saqlash",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: _previousTrack,
-                icon: Icon(Icons.skip_previous, color: Colors.white),
-                iconSize: 36,
-              ),
-              IconButton(
-                onPressed: _playPause,
-                icon: Icon(
-                  _isPlaying
-                      ? Icons.pause_circle_filled
-                      : Icons.play_circle_filled,
-                  color: Colors.white,
-                ),
-                iconSize: 64,
-              ),
-              IconButton(
-                onPressed: _nextTrack,
-                icon: Icon(Icons.skip_next, color: Colors.white),
-                iconSize: 36,
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
         ],
       ),
     );
   }
 
-  String _formatDuration(Duration duration) {
-    String minutes = duration.inMinutes.toString().padLeft(2, '0');
-    String seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
-    return "$minutes:$seconds";
+  Widget _buildTextField(String hint,
+      {bool isPassword = false, TextEditingController? controller}) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.white),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.2),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      style: TextStyle(color: Colors.white),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  final String firstName;
+  final String lastName;
+
+  LoginScreen({required this.firstName, required this.lastName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$firstName $lastName"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF4A00E0),
+                  Color(0xFF8E2DE2),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      _buildTextField("Login"),
+                      SizedBox(height: 10),
+                      _buildTextField("Parol", isPassword: true),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CurrencyScreen(
+                          name: "Abdulloh",
+                          surname: "Akramov",
+                        ),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  ),
+                  child: Text(
+                    "Kirish",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField(String hint, {bool isPassword = false}) {
+    return TextField(
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.black54),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      style: TextStyle(color: Colors.black),
+    );
+  }
+}
+
+class CurrencyScreen extends StatefulWidget {
+  final String name;
+  final String surname;
+
+  CurrencyScreen({required this.name, required this.surname});
+
+  @override
+  _CurrencyScreenState createState() => _CurrencyScreenState();
+}
+
+class _CurrencyScreenState extends State<CurrencyScreen> {
+  List<dynamic> currencies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCurrencyData();
+  }
+
+  Future<void> fetchCurrencyData() async {
+    final response = await http
+        .get(Uri.parse('https://cbu.uz/uz/arkhiv-kursov-valyut/json/'));
+    if (response.statusCode == 200) {
+      setState(() {
+        currencies = jsonDecode(response.body);
+      });
+    } else {
+      // Handle error
+      throw Exception('Failed to load currency data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("${widget.surname} ${widget.name}"),
+            Text(
+              TimeOfDay.now().format(context),
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+      body: currencies.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: currencies.length,
+              itemBuilder: (context, index) {
+                final currency = currencies[index];
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Yaqinlangan kun: ${currency['Date']}",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "1 ${currency['Ccy']} (${currency['CcyNm_UZ']})",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "${currency['Rate']}",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "${currency['Diff']}",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: double.parse(currency['Diff']) > 0
+                                    ? Colors.green
+                                    : Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
